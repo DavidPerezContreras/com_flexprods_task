2c014a065d3be4c3f62c8e837d44892f93c323b6
DavidPerezContreras <david.perez.dam2@gmail.com>
2024-03-27 07:54:09 +0100

    web-test

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://task.flexprods.com";
 
 final Uri linkedInUrl = Uri.parse(
     'https://www.linkedin.com/in/david-p%C3%A9rez-contreras');

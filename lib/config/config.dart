5b777c9153b163342706f755e024bf9f2dd8c5ba
DavidPerezContreras <david.perez.dam2@gmail.com>
2024-06-10 20:25:57 +0200

    port 443

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
-const baseUrl = "https://task-api.davidperezcontreras.com";
+const baseUrl = "https://task-api.davidperezcontreras.com:443";
 
 final Uri linkedInUrl = Uri.parse(
     'https://es.linkedin.com/in/david-perez-contreras');
